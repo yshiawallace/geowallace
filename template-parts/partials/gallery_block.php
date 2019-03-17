80dc08bf6b29aa1e9ccf7e0584cd7897f3c74640
Yshia Wallace <yshiawallace@gmail.com>
2019-03-17 18:50:43 -0400

    Add H6 style and title to gallery block

--- template-parts/partials/gallery_block.php
+++ template-parts/partials/gallery_block.php
@@ -1,41 +1,45 @@
 <?php 
     $gallery_images = get_sub_field('gallery');
+    $gallery_title = get_sub_field('title');
 ?>        
         <div class="block block--gallery">
+        <?php if( $gallery_title ) : ?>
+            <h2 class="gallery-title"><?php echo $gallery_title; ?></h2>
+        <?php endif; ?>
             <div class="gallery">
-            <?php
-                $count = 0;
-                foreach( $gallery_images as $image ) :
-                    $img_id = $image['ID'];
-                    $url = $image['url'];
-                    $alt = $image['alt'];
-                    $width = $image['width'];
-                    $height = $image['height'];
-                    $size = 'full';
-                    $img = wp_get_attachment_image($img_id, $size);
-                    $thumbnail = wp_get_attachment_image($img_id, 'thumbnail');
+        <?php
+            $count = 0;
+            foreach( $gallery_images as $image ) :
+                $img_id = $image['ID'];
+                $url = $image['url'];
+                $alt = $image['alt'];
+                $width = $image['width'];
+                $height = $image['height'];
+                $size = 'full';
+                $img = wp_get_attachment_image($img_id, $size);
+                $thumbnail = wp_get_attachment_image($img_id, 'thumbnail');
 
-                    if( $count === 0 ) :
-                ?>
-                    <figure class="gallery__item gallery__item--first" itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
-                        <a href="<?php echo $url; ?>" data-size="<?php echo $width . 'x' . $height; ?>"><?php echo $thumbnail; ?></a>
-                        <?php if( $alt ) : ?>
-                            <figcaption itemprop="caption description"><?php echo $alt; ?></figcaption>
-                        <?php endif; ?>
-                    </figure>
-                <?php
-                    else :
-                ?>  
-                    <figure class="gallery__item" itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
-                        <a href="<?php echo $url; ?>" data-size="<?php echo $width . 'x' . $height; ?>"><?php echo $thumbnail; ?></a>
-                        <?php if( $alt ) : ?>
-                            <figcaption itemprop="caption description"><?php echo $alt; ?></figcaption>
-                        <?php endif; ?>
-                    </figure>          
-            <?php
-                    endif;
-                    $count++;
-                endforeach;
+                if( $count === 0 ) :
             ?>
+                <figure class="gallery__item gallery__item--first" itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+                    <a href="<?php echo $url; ?>" data-size="<?php echo $width . 'x' . $height; ?>"><?php echo $thumbnail; ?></a>
+                    <?php if( $alt ) : ?>
+                        <figcaption itemprop="caption description"><?php echo $alt; ?></figcaption>
+                    <?php endif; ?>
+                </figure>
+            <?php
+                else :
+            ?>  
+                <figure class="gallery__item" itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+                    <a href="<?php echo $url; ?>" data-size="<?php echo $width . 'x' . $height; ?>"><?php echo $thumbnail; ?></a>
+                    <?php if( $alt ) : ?>
+                        <figcaption itemprop="caption description"><?php echo $alt; ?></figcaption>
+                    <?php endif; ?>
+                </figure>          
+        <?php
+                endif;
+                $count++;
+            endforeach;
+        ?>
             </div>
         </div>
